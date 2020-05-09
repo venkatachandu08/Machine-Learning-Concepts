b9bcbc5de8a5fad51368756c5f840aae4beb7c27
Konduru Venkata Chandu <58496589+venkatachandu08@users.noreply.github.com>
2020-05-09 21:16:33 +0530

    Add files via upload

---
+++ prac_polynomial_regression.py
@@ -0,0 +1,92 @@
+# Data Preprocessing Template
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values
+
+#plot
+plt.plot(X,y)
+
+#Fitting Linear Regression into dataset
+from sklearn.linear_model import LinearRegression
+lin_reg=LinearRegression()
+lin_reg.fit(X,y)
+
+#Fitting Polynomial Regression to dataset
+from sklearn.preprocessing import PolynomialFeatures
+poly_reg= PolynomialFeatures(degree=5)
+x_poly=poly_reg.fit_transform(X)
+lin_reg2=LinearRegression()
+lin_reg2.fit(x_poly,y)
+
+#Visualizing Linear Regression results
+plt.scatter(X, y, color='red')
+plt.plot(X, lin_reg.predict(X), color='blue')
+plt.title('Truth or Bluff(Linear Regression)')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
+
+#Visualizing Polynomial Regression results
+x_grid=np.arange(min(X),max(X),0.1)
+x_grid=x_grid.reshape((len(x_grid),1))
+plt.scatter(X, y, color='red')
+plt.plot(x_grid, lin_reg2.predict(poly_reg.fit_transform(x_grid)), color='blue')
+plt.title('Truth or Bluff(Linear Regression)')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
+
+#Predicting a new Result with linear Regression
+lin_reg.predict(np.array([[6.5]]))
+#array([330378.78787879])
+
+#Predicting a new Result with Polynomial Regression
+lin_reg2.predict(poly_reg.fit_transform([[6.5]]))
+#array([174878.07765118])
+
+
+
+
+"""# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+"""
+#Encoding Categorical Data in Spyder3
+"""from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:, 3] = labelencoder_X.fit_transform(X[:, 3])
+onehotencoder = OneHotEncoder()
+X = onehotencoder.fit_transform(X).toarray()"""
+
+#Encoding Categorical Data in Spyder4
+"""from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+labelencoder_X = LabelEncoder()
+X[:, 3] = labelencoder_X.fit_transform(X[:, 3])
+transformer = ColumnTransformer(
+    transformers=[
+        ("OneHot",        # Just a name
+         OneHotEncoder(), # The transformer class
+         [3]              # The column(s) to be applied on.
+         )
+    ],
+    remainder='passthrough' # donot apply anything to the remaining columns
+)
+X = transformer.fit_transform(X.tolist())
+X = X.astype('float64')"""
+
+
+# Feature Scaling
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+sc_y = StandardScaler()
+y_train = sc_y.fit_transform(y_train.reshape(-1,1))"""
