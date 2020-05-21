f0651d57a66e1957106e2adab2ead6b426fbbef5
Konduru Venkata Chandu <58496589+venkatachandu08@users.noreply.github.com>
2020-05-21 22:50:26 +0530

    Decision Tree Regression

--- prac_decision_tree_regression.py
+++ prac_decision_tree_regression.py
@@ -52,3 +52,22 @@ plt.title('Truth or Bluff( Decision Regression Model)')
 plt.xlabel('Position Level')
 plt.ylabel('Salary')
 plt.show()
+
+
+
+#Encoding Categorical Data in Spyder4
+"""from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+labelencoder_X = LabelEncoder()
+X[:, 3] = labelencoder_X.fit_transform(X[:, 3])
+transformer = ColumnTransformer(
+    transformers=[
+        ("OneHot",        # Just a name
+         OneHotEncoder(), # The transformer class
+         [3]              # The column(s) to be applied on.
+         )
+    ],
+    remainder='passthrough' # donot apply anything to the remaining columns
+)
+X = transformer.fit_transform(X.tolist())
+X = X.astype('float64')"""
