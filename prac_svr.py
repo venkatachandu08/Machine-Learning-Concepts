85f091f3e3c4ac1f519e064e0525db1c913d06e1
Konduru Venkata Chandu <58496589+venkatachandu08@users.noreply.github.com>
2020-05-22 22:27:38 +0530

    SVR

--- prac_svr.py
+++ prac_svr.py
@@ -86,4 +86,6 @@ transformer = ColumnTransformer(
 X = transformer.fit_transform(X.tolist())
 X = X.astype('float64')"""
 
-
+"""# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)"""
